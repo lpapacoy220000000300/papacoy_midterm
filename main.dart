a0ef05ae869aadf8142bf4bdd2240810f51f38a2
Lorenz Eric Papacoy <142190662+lpapacoy220000000300@users.noreply.github.com>
2024-10-24 23:40:22 +0800

    Update main.dart

--- main.dart
+++ main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:paps/screens/home_screen.dart';
 import 'package:paps/screens/input_screen.dart';
 import 'package:paps/screens/list_screen.dart';
+import 'package:paps/screens/edit_screen.dart';
 
 void main() {
   runApp(InventoryApp());
